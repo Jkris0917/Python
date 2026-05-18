fc2bc45b44855fc0bd1c1f373ce80b89eeaad78e
Jkris0917 <gelladojohnkris@gmail.com>
2026-05-18 17:45:27 +0800

    Week 1 Training Data Analyst

---
+++ calculator/calculator.py
@@ -0,0 +1,28 @@
+guest_name = input("Enter your Name: ")
+print(f"Hi!, {guest_name}.")
+print()
+print("====Welcome to Calculator!====")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+power = num1 ** num2
+
+
+print(f"\nSum of {num1} and {num2} is: {add} ")
+print(f"\nDifference of {num1} and {num2} is: {subtract} ")
+if num2 != 0:
+    print(f"\nQuotient of {num1} and {num2} is: {num1 / num2:.2f} ")
+else:
+    print("Can`t be divided by Zero!")
+
+print(f"\nProduct of {num1} and {num2} is: {multiply} ")
+print(f"\nPower of {num1} and {num2} is: {power} ")
+if num2 != 0:
+    print(f"\nRemainder of {num1} and {num2} is: {num1 % num2:.2f} ")
+else:
+    print("Can`t be divided by Zero!")
+
