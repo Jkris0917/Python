a7ab317f1b3c2af398d98fbc0fb3269d07cf81d9
Jkris0917 <gelladojohnkris@gmail.com>
2026-05-20 17:19:58 +0800

    Update Contact Book

--- contact_book/contact_book.py
+++ contact_book/contact_book.py
@@ -1,33 +1,47 @@
-from contact_func import show_contacts,add_contacts
+from contact_func import show_contacts,add_contacts,update_contacts,search_contact,delete_contacts
 
 def main():
     contacts = {}
     print("==Contact Book==")
-    print("""[1] - Show All\n[2] - Add\n[3] - Update\n[4] - Search\n[5] - Delete\n[6] - Quit""")
+    
     
     while True:
+        print("""[1] - Show All\n[2] - Add\n[3] - Update\n[4] - Search\n[5] - Delete\n[6] - Quit""")
         choose = input("Choose: ")
         if not choose.isdigit():
             print("Must be a number")
+            continue
         choose = int(choose)
         
         if choose == 1:
             show_contacts(contacts)
             
         elif choose == 2:
-            name = input("Enter Name: ")  
+            name = input("Enter Name: ").lower() 
             phone = input("Enter Phone: ")  
             email = input("Enter Email: ")
             add_contacts(contacts,name,phone,email)
             
         elif choose == 3:
-            pass
+            show_contacts(contacts)
+            print("Select Name of Contact you want to update")
+            try:
+                name = input("Enter Name: ").lower()
+                if name == '':
+                    print('Provide name')
+                phone = input("Enter Phone:")
+                email = input("Enter Email: ")
+                update_contacts(contacts,name,phone,email)
+            except ValueError:
+                print("Invalid!")
         
         elif choose == 4:
-            pass
+            name = input("Enter Name: ").lower()
+            search_contact(contacts, name)
         
         elif choose == 5:
-            pass
+            name = input("Enter Name: ").lower()
+            delete_contacts(contacts, name)
         
         elif choose == 6:
             print("Goodbye!")
--- contact_book/contact_func.py
+++ contact_book/contact_func.py
@@ -2,6 +2,7 @@ def show_contacts(contacts):
     """Show list of contacts"""
     if not contacts:
         print("No contacts yet")
+        return
     
     for name,info in contacts.items():
         print(f"\n{name.title()}")
@@ -11,4 +12,35 @@ def show_contacts(contacts):
 def add_contacts(contacts,name,phone,email):
     """Add a contacts"""
     contacts[name]= {"phone":phone,"email":email}
-    print(f"'{name}' Added successfully!")
+    print(f"'{name}' added successfully!")
+    
+def update_contacts(contacts,name,phone,email):
+    """Update a contact"""
+    if name not in contacts:
+        print(f"'{name}' not in the contact list")
+        return
+    
+    if phone.strip() == '':
+        contacts[name]["phone"] = phone
+
+    if email.strip() == '':
+        contacts[name]['email'] = email
+
+    print(f"'{name}' updated successfully!")
+    
+def search_contact(contacts, name):
+    """Search a contact"""
+    if name not in contacts:
+        print(f"'{name}' is not in contact list")
+    else:
+        print(name.title())
+        print(f"Phone: {contacts[name]['phone']}")
+        print(f"Email: {contacts[name]['email']}")
+    
+def delete_contacts(contacts, name):
+    if name not in contacts:
+        print(f"'{name}' is not in contact list")
+        return
+
+    contacts.pop(name)
+    print(f"'{name}' deleted successfully!")
